1bce7b356218e49183fd0be48d7935c04ccc98b9
rain9155 <jianyu9155@gmail.com>
2019-05-27 23:17:34 +0800

    first commit

---
+++ app/src/main/java/com/example/p2p/ChatActivity.java
@@ -0,0 +1,13 @@
+package com.example.p2p;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
---
+++ app/src/main/java/com/example/p2p/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.p2p;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startActivity(new Intent(this, ChatActivity.class));
+    }
+}
